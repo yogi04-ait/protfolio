4375d69dc5a83cb4c7715d2594c17936fac2fa31
yogesh aithani <aithaniyogesh007@gmail.com>
2024-01-23 18:16:41 +0530

    responsive site

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
-const menubar = document.querySelector('.ham-brg');
+const btnElem = document.querySelector('.button');
 
-menubar.addEventListener('click', function () {
+document.querySelector('.ham-brg').addEventListener('click', function(){
+    const currentDisplay = window.getComputedStyle(btnElem).getPropertyValue('display');
+    btnElem.style.display = currentDisplay === 'none' ? 'flex' : 'none';
+})
 
-  const buttonElement = document.querySelector('.button');
-  // Check the current display property
-  const currentDisplay = window.getComputedStyle(buttonElement).getPropertyValue('display');
 
-  // Toggle the display property
-  buttonElement.style.display = currentDisplay === 'none' ? 'flex' : 'none';
 
-});
+// Function to be executed when screen width is equal or above 760 pixels
+function executeOnScreenWidth760OrAbove() {
+    btnElem.style.display = 'block';
+    // Example: Change the background color of the body
+  }
+
+function executeOnScreenWidth760below(){
+    btnElem.style.display = 'flex';
+    btnElem.style.display = 'none';
+
+}
+  
+  // Check screen width when the page loads
+  window.addEventListener('load', function () {
+    const screenWidth = window.innerWidth;
+  
+    // Check if the screen width is equal to or above 760 pixels
+    if (screenWidth >= 760) {
+      executeOnScreenWidth760OrAbove();
+    }
+
+    if(screenWidth < 760){
+        executeOnScreenWidth760below();
+    }
+  });
+  
+  // Check screen width whenever the window is resized
+  window.addEventListener('resize', function () {
+    const screenWidth = window.innerWidth;
+  
+    // Check if the screen width is equal to or above 760 pixels
+    if (screenWidth >= 760) {
+      executeOnScreenWidth760OrAbove();
+    }
+    if(screenWidth < 760){
+        executeOnScreenWidth760below();
+    }
+  });
+  
+
+
+
+  
